51fafdf8f76f40d56a328ad8297ec41654c9268f
Bhuvaneshwaran S <bhuvaneshsaravanan100@gmail.com>
2025-12-24 08:11:52 +0530

    Update emailjs environment variables for Vite

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,10 +12,10 @@ function Contact() {
     e.preventDefault();
 
     emailjs.sendForm(
-      process.env.REACT_APP_EMAILJS_SERVICE_ID,
-      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      process.env.VITE_EMAILJS_SERVICE_ID,
+      process.env.VITE_EMAILJS_TEMPLATE_ID,
       form.current,
-      process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      process.env.VITE_EMAILJS_PUBLIC_KEY
     )
       .then(
         (result) => {
